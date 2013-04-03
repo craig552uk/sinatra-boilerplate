59a624ff35a72e085dbd75b0ef2b5f7038d28758
Craig Russell <craig@craig-russell.co.uk>
2013-04-03 12:23:59 +0100

    Only load application helpers is Application exists

--- helpers/init.rb
+++ helpers/init.rb
@@ -7,6 +7,8 @@ require_relative 'reserved_words' # Extends String class
 require_relative 'mime_type'
 require_relative 'session'
 
-Application.helpers RouteHelpers
-Application.helpers ViewHelpers
-Application.helpers Session
+if defined?(Application) == 'constant' && Application.class == Class
+  Application.helpers RouteHelpers
+  Application.helpers ViewHelpers
+  Application.helpers Session
+end
